047b861cbdaadc801c7fcb2597d3bec62e4ccbbc
thiagofdiniz <thiago.diniz2@etec.sp.gov.br>
2019-09-05 01:42:00 -0300

    Alterações na pagina de Login

--- app/src/main/java/br/com/etecia/appestrela/MainActivity.java
+++ app/src/main/java/br/com/etecia/appestrela/MainActivity.java
@@ -1,13 +1,25 @@
 package br.com.etecia.appestrela;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageView ImgInicio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImgInicio = (ImageView)findViewById(R.id.ImgInicio);
+        ImgInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Login_Activity.class));
+                finish();
+            }
+        });
     }
 }
