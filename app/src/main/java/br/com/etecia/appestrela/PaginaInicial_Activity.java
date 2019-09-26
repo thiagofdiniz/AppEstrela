6fed5a7b1114f8d8cf9b5ba8bce7074be064314a
thiagofdiniz <Edimilson@1>
2019-09-25 21:06:38 -0300

    Inserido o menu

--- app/src/main/java/br/com/etecia/appestrela/PaginaInicial_Activity.java
+++ app/src/main/java/br/com/etecia/appestrela/PaginaInicial_Activity.java
@@ -2,12 +2,20 @@ package br.com.etecia.appestrela;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class PaginaInicial_Activity extends AppCompatActivity {
-
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pagina_inicial_layout);
+
+        toolbar = findViewById(R.id.idToolBar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_android_black);
+
     }
 }
